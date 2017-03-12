0b68522c77bb502f5029615b326500b21d66d773
Paul Filiora <paul.filiora@gmail.com>
2017-03-12 17:31:33 -0400

    del and del completed implemented

---
+++ index.js
@@ -0,0 +1,94 @@
+//include express
+const express = require('express');
+
+// grab db
+const low = require('lowdb');
+
+// static file server
+const serveStatic = require('serve-static');
+
+// body parser middleware
+const parser = require('body-parser');
+
+//create an express application
+const app = express();
+
+// instantiate db
+const db = low('./db.json');
+
+//parses requests with the content type of `application/json`
+app.use(parser.json());
+
+//define a route on `/hello/world`
+app.get('/api/todos',(request, response) => {
+    response.header('Content-Type', 'application/json');
+    response.send(db.get('todos').value());
+});
+
+
+// POST create new todos
+app.post('/api/todos', (request, response) => {
+	const requestBody = request.body;
+
+	// Add a post
+	db.get('todos').push({
+		id: Date.now(),
+		data: requestBody,
+	}).write();
+
+	response.header('Content-Type', 'application/json');
+	response.send(db.get('todos').value());
+});// POST create new todos
+
+
+// PUT ID in todos
+app.put('/api/todo/:id', (request, response) => {
+	const dataPayload = request.body;
+  const id = parseInt(request.params.id, 10);
+
+  Object.keys(request.body).forEach((key) => {
+    console.log(key, request.body[key])
+    db.get('todos')
+      .find({ id })
+      .set('data.' + key, request.body[key])
+      .write();
+
+    console.log(db.get('todos').find({id}).value())
+  })
+	response.header('Content-Type', 'application/json');
+	response.send(db.get('todos').value());
+});
+
+// DELETE todos
+app.delete('/api/todo/:id', (request, response) => {
+  const id = parseInt(request.params.id, 10);
+
+  db.get('todos')
+    .remove({ id })
+    .write()
+
+	response.header('Content-Type', 'application/json');
+	response.send(db.get('todos').value());
+
+});// DELETE todos
+
+//DELETE COMPLETED todos
+app.delete('/api/todos/completed', (request, response) => {
+    db.get('todos')
+      .remove(todo => {
+          return todo.data.isDone === true
+      })
+      .write();
+
+      response.header('Content-Type', 'application/json');
+    	response.send(db.get('todos').value());
+})//DELETE COMPLETED todos
+
+app.use('/', serveStatic( 'public', {
+	'index': [ 'index.html' ]
+}));
+
+//have the application listen on a specific port
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
---
+++ public/javascript/main.js
@@ -0,0 +1,233 @@
+(function() { // protect the lemmings
+
+    function GET(url) {
+        return new Promise((resolve, reject) => {
+            const request = new XMLHttpRequest();
+            request.open('GET', url);
+            request.onload = () => {
+                const data = JSON.parse(request.responseText);
+                resolve(data)
+            };
+            request.onerror = (err) => {
+                reject(err)
+            };
+            request.send();
+        });
+    } // GET
+
+    function POST(url, data) {
+        return new Promise((resolve, reject) => {
+            const request = new XMLHttpRequest();
+            request.open('POST', url);
+            request.setRequestHeader('Content-Type', 'application/json');
+
+            request.onload = () => {
+                const data = JSON.parse(request.responseText);
+                resolve(data)
+            };
+            request.onerror = (err) => {
+                reject(err)
+            };
+
+            request.send(JSON.stringify(data));
+        });
+    } // POST
+
+    function PUT(url, data) {
+        return new Promise((resolve, reject) => {
+            const request = new XMLHttpRequest();
+            request.open('PUT', url);
+            request.setRequestHeader('Content-Type', 'application/json');
+
+            request.onload = () => {
+                const data = JSON.parse(request.responseText);
+                resolve(data)
+            };
+            request.onerror = (err) => {
+                reject(err)
+            };
+
+            request.send(JSON.stringify(data));
+        });
+    } // PUT
+
+    function DELETE(url, data = {}) {
+        return new Promise((resolve, reject) => {
+            const request = new XMLHttpRequest();
+            request.open('DELETE', url);
+            request.setRequestHeader('Content-Type', 'application/json');
+
+            request.onload = () => {
+                const data = JSON.parse(request.responseText);
+                resolve(data)
+            };
+            request.onerror = (err) => {
+                reject(err)
+            };
+
+            request.send(JSON.stringify(data));
+        });
+    } // DELETE
+
+    function render(todoItems) {
+        const container = document.querySelector('.js-todolist');
+        container.innerHTML = '';
+        for (const todoItem of todoItems) {
+            const li = document.createElement('li');
+            li.innerHTML = `
+<input type="text" class="todoinput" value="${todoItem.data.todo}">
+			`;
+
+            // li.innerHTML += `<span class="glyphicon glyphicon-remove todolist-icon js-todo-remove"></span>`;
+            li.innerHTML += `<i class="fa fa-trash-o todolist-icon trash-icon js-todo-remove"></i>`
+
+            if (todoItem.data.isDone) {
+                li.innerHTML += `<span class="glyphicon glyphicon-check check-icon todolist-icon js-todo-check"></span>`
+                li.className += `todolist-item-done`
+            } else {
+                li.innerHTML += `<span class="glyphicon glyphicon-unchecked check-icon todolist-icon js-todo-check"></span>`
+            }
+
+
+            li.classList.add('list-group-item', 'todolist-item');
+
+            container.appendChild(li);
+            li.querySelector('.js-todo-remove').addEventListener('click', (e) => {
+                console.log('about to delete LOL')
+                const {
+                    id
+                } = todoItem;
+
+                DELETE('/api/todo/' + id)
+                    .then((data) => {
+                        render(data);
+                    })
+                    .catch((e) => {
+                        alert(e)
+                    });
+            });
+            const changeInput= li.querySelector('.todoinput')
+            changeInput.addEventListener('keydown', (e) => {
+                const {
+                    keyCode,
+                    which
+                } = e;
+
+                if (keyCode === 13 || which === 13) {
+                    console.log('updater');
+                    const update = changeInput.value
+                    PUT('/api/todo/' + todoItem.id, {
+                            todo: e.target.value,
+                            isDone: false,
+                        })
+                        .then((data) => {
+                            render(data);
+                        })
+                        .catch((e) => {
+                            alert(e)
+                        })
+
+                        // POST('/api/todos', {
+                        //     todo: input.value,
+                        //     when: new Date().getTime() + 9 * 60 * 60 * 1000
+                        // }).then((data) => {
+                        //     input.removeAttribute('disabled');
+                        //     input.value = '';
+                        //     render(data);
+                        // })
+                }
+            });
+            li.querySelector('.js-todo-check').addEventListener('click', (e) => {
+                // console.log(todoItem);
+                let isDone;
+                if (todoItem.data.isDone) {
+                    isDone = false;
+                } else {
+                    isDone = true;
+                }
+
+                PUT('/api/todo/' + todoItem.id, {
+                        isDone
+                    })
+                    .then((data) => {
+                        render(data);
+                    })
+                    .catch((e) => {
+                        alert(e)
+                    })
+            })
+        }
+
+        if (todoItems.length === 0) {
+            container.innerHTML = `
+<li class="list-group-item">
+No todoitems!
+</li>
+			`;
+        }
+    } // render
+
+
+    GET('/api/todos')
+        .then((todoItems) => {
+            render(todoItems);
+        });
+
+    const addToDo = () => {
+        const input = document.querySelector('.js-todo-text');
+        input.setAttribute('disabled', 'disabled');
+        POST('/api/todos', {
+            todo: input.value,
+            when: new Date().getTime() + 9 * 60 * 60 * 1000
+        }).then((data) => {
+            input.removeAttribute('disabled');
+            input.value = '';
+            render(data);
+        });
+    }
+
+    document.querySelector('.js-todo-text').addEventListener('keydown', (e) => {
+        const {
+            keyCode,
+            which
+        } = e;
+        if (keyCode === 13 || which === 13) {
+            addToDo();
+        }
+    });
+
+    document.querySelector('.js-add-todo').addEventListener('click', (e) => addToDo())
+
+    document.querySelector('.js-clear-completed').addEventListener('click', (e) => {
+        console.log('clear completed')
+        // todoItems.forEach((item) => {
+        //   if (item.data.isDone === true) {
+        //     DELETE('/api/todo/' + item.id)
+        //         .then((data) => {
+        //             render(data);
+        //         })
+        //         .catch((e) => {
+        //             alert(e)
+        //         });
+        //   }
+        // })
+        DELETE('/api/todos/completed')
+          .then((data) => {
+            render(data)
+          })
+          .catch((e) => {
+            alert(e)
+          })
+    });
+
+})();
+
+/*
+	return POST('/api/todos', {
+			todo: 'wake up',
+			when: new Date().getTime() + 9 * 60 * 60 * 1000
+		});
+	.then((dataFromPostLOL) => {
+		console.log(dataFromPostLOL);
+	});
+*/
